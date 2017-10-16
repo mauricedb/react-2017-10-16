570574aff8ef72bfb24dc5d35dff4f0a775ca4c6
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-10-16 15:18:32 +0100

    Before demo

---
+++ demo-03/src/Cat.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Cat extends Component {
+  render() {
+    return (
+      <div>
+        <img src="/cat.jpg" width="600" />
+      </div>
+    );
+  }
+}
+
+export default Cat;
