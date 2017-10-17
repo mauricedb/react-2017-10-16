7d90edc25475134b71261333ab4fd51e4cccd754
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-10-17 10:07:28 +0100

    Before Redux

---
+++ demo-05/src/Counter.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <div>Count: NaN</div>
+        <button>Increment</button>
+        <button>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
