fde666d8e64d76159a50922b08c33a3a637ee197
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-10-18 17:00:09 +0100

    Advanced TypeScript

--- demo-10/index.js
+++ demo-10/index.js
@@ -1,69 +1,124 @@
-var xEl = document.getElementById("x");
-var yEl = document.getElementById("y");
-var resultEl = document.getElementById("result");
-var addEl = document.getElementById("add");
-var subtractEl = document.getElementById("subtract");
-var Options = /** @class */ (function () {
-    function Options() {
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const xEl = document.getElementById("x");
+const yEl = document.getElementById("y");
+const resultEl = document.getElementById("result");
+const addEl = document.getElementById("add");
+const subtractEl = document.getElementById("subtract");
+if (addEl && subtractEl) {
+    class Options {
     }
-    return Options;
-}());
-var Calculator = /** @class */ (function () {
-    function Calculator(resultEl) {
-        this.resultEl = resultEl;
+    class Calculator {
+        constructor(resultEl) {
+            this.resultEl = resultEl;
+        }
+        add(x, y) {
+            return x + y;
+        }
+        subtract(x, y) {
+            return x - y;
+        }
+        print(result) {
+            if (this.resultEl) {
+                this.resultEl.textContent = result.toString();
+            }
+        }
     }
-    Calculator.prototype.add = function (x, y) {
-        return x + y;
-    };
-    Calculator.prototype.subtract = function (x, y) {
-        return x - y;
-    };
-    Calculator.prototype.print = function (result) {
-        this.resultEl.textContent = result;
-    };
-    return Calculator;
-}());
-var calculator = new Calculator(resultEl);
-addEl.addEventListener("click", function () {
-    var x = +xEl.value;
-    var y = +yEl.value;
-    var result = calculator.add(x, y);
-    calculator.print(result);
-});
-subtractEl.addEventListener("click", function () {
-    var x = +xEl.value;
-    var y = +yEl.value;
-    var result = calculator.subtract(x, y);
-    calculator.print(result);
-});
-var Person = /** @class */ (function () {
-    function Person() {
+    const calculator = new Calculator(resultEl);
+    addEl.addEventListener("click", () => {
+        const x = +xEl.value;
+        const y = +yEl.value;
+        const result = calculator.add(x, y);
+        calculator.print(result);
+    });
+    subtractEl.addEventListener("click", () => {
+        const x = +xEl.value;
+        const y = +yEl.value;
+        const result = calculator.subtract(x, y);
+        calculator.print(result);
+    });
+    class Person {
     }
-    return Person;
-}());
-var Cat = /** @class */ (function () {
-    function Cat() {
-        this.type = "cat";
+    class Cat {
+        sleep() { }
     }
-    Cat.prototype.sleep = function () { };
-    return Cat;
-}());
-var Dog = /** @class */ (function () {
-    function Dog() {
-        this.type = "dog";
+    class Dog {
+        bark() { }
     }
-    Dog.prototype.bark = function () { };
-    return Dog;
-}());
-function printIt(o) {
-    console.log(o.name);
+    function useAnimal(animal) {
+        console.log(animal.name);
+        switch (animal.type) {
+            case "dog":
+                animal.bark();
+                break;
+            case "cat":
+                animal.sleep();
+                break;
+        }
+        if (animal instanceof Cat) {
+            animal.sleep();
+        }
+    }
+    useAnimal({
+        type: "cat",
+        name: "",
+        sleep() { }
+    });
+    function printIt(o) {
+        if (!o)
+            return;
+        console.log(o.name);
+    }
+    function create(o) {
+        if (o)
+            return new Person();
+        return new Cat();
+    }
+    printIt(new Person());
+    printIt(create(true));
+    printIt(null);
+    // printIt(undefined);
+    function doAsyncStuff() {
+        return new Promise((resolve, reject) => setTimeout(() => {
+            try {
+                // throw new Error("THis should not happen");
+                resolve(42);
+            }
+            catch (err) {
+                reject(err);
+            }
+        }, 2000));
+    }
+    function oldWay() {
+        console.log("Before request");
+        doAsyncStuff()
+            .then(e => console.log(e))
+            .catch(err => console.error(err));
+        console.log("After request");
+    }
+    function newWay() {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                console.log("Before request");
+                var result1 = doAsyncStuff();
+                var result2 = doAsyncStuff();
+                console.log("After request before print");
+                console.log(yield result1);
+                console.log(yield result2);
+                console.log("After request");
+            }
+            catch (err) {
+                console.error("OOps", err);
+            }
+        });
+    }
+    // oldWay()
+    newWay();
 }
-function create(o) {
-    if (o)
-        return new Person();
-    return new Cat();
-}
-printIt(new Person());
-printIt(create(true));
-printIt(null);
-printIt(undefined);
--- demo-10/index.ts
+++ demo-10/index.ts
@@ -5,74 +5,142 @@ const resultEl = document.getElementById("result");
 const addEl = document.getElementById("add");
 const subtractEl = document.getElementById("subtract");
 
-class Options {
-  prefix: string;
-}
-
-class Calculator {
-  constructor(private readonly resultEl) {}
-
-  add(x, y) {
-    return x + y;
-  }
-  subtract(x, y) {
-    return x - y;
+if (addEl && subtractEl) {
+  class Options {
+    prefix: string;
   }
 
-  print(result) {
-    this.resultEl.textContent = result;
+  class Calculator {
+    constructor(private readonly resultEl: HTMLElement | null) {}
+
+    add(x: number, y: number) {
+      return x + y;
+    }
+    subtract(x: number, y: number) {
+      return x - y;
+    }
+
+    print(result: number) {
+      if (this.resultEl) {
+        this.resultEl.textContent = result.toString();
+      }
+    }
   }
+
+  const calculator = new Calculator(resultEl);
+
+  addEl.addEventListener("click", () => {
+    const x = +xEl.value;
+    const y = +yEl.value;
+
+    const result = calculator.add(x, y);
+    calculator.print(result);
+  });
+
+  subtractEl.addEventListener("click", () => {
+    const x = +xEl.value;
+    const y = +yEl.value;
+
+    const result = calculator.subtract(x, y);
+    calculator.print(result);
+  });
+
+  class Person {
+    name: string;
+    age: null;
+  }
+
+  class Cat {
+    type: "cat";
+    name: string;
+    sleep() {}
+  }
+
+  class Dog {
+    type: "dog";
+    name: string;
+    bark() {}
+  }
+
+  function useAnimal(animal: Cat | Dog) {
+    console.log(animal.name);
+
+    switch (animal.type) {
+      case "dog":
+        animal.bark();
+        break;
+      case "cat":
+        animal.sleep();
+        break;
+    }
+
+    if (animal instanceof Cat) {
+      animal.sleep();
+    }
+  }
+
+  useAnimal({
+    type: "cat",
+    name: "",
+    sleep() {}
+  });
+
+  interface HasName {
+    name: string;
+  }
+
+  function printIt(o: HasName | null) {
+    if (!o) return;
+
+    console.log(o.name);
+  }
+
+  function create(o: boolean): Person | Cat {
+    if (o) return new Person();
+    return new Cat();
+  }
+
+  printIt(new Person());
+  printIt(create(true));
+  printIt(null);
+  // printIt(undefined);
+
+  function doAsyncStuff() {
+    return new Promise((resolve, reject) =>
+      setTimeout(() => {
+        try {
+          // throw new Error("THis should not happen");
+          resolve(42);
+        } catch (err) {
+          reject(err);
+        }
+      }, 2000)
+    );
+  }
+
+  function oldWay() {
+    console.log("Before request");
+    doAsyncStuff()
+      .then(e => console.log(e))
+      .catch(err => console.error(err));
+    console.log("After request");
+  }
+
+  async function newWay() {
+    try {
+      console.log("Before request");
+      var result1 = doAsyncStuff();
+      var result2 = doAsyncStuff();
+      console.log("After request before print");
+      console.log(await result1);
+      console.log(await result2);
+      console.log("After request");
+    } catch (err) {
+      console.error("OOps", err);
+    }
+  }
+
+  // oldWay()
+
+  newWay();
 }
-
-const calculator = new Calculator(resultEl);
-
-addEl.addEventListener("click", () => {
-  const x = +xEl.value;
-  const y = +yEl.value;
-
-  const result = calculator.add(x, y);
-  calculator.print(result);
-});
-
-subtractEl.addEventListener("click", () => {
-  const x = +xEl.value;
-  const y = +yEl.value;
-
-  const result = calculator.subtract(x, y);
-  calculator.print(result);
-});
-
-class Person {
-  name: string;
-  age: null;
-}
-
-class Cat {
-  type = "cat";
-  name: string;
-  sleep() {}
-}
-
-class Dog {
-  type = "dog";
-  name: string;
-  bark() {}
-}
-
-interface HasName {
-  name: string;
-}
-
-function printIt(o: HasName) {
-  console.log(o.name);
-}
-
-function create(o: boolean): Person | Cat {
-  if (o) return new Person();
-  return new Cat();
-}
-
-printIt(new Person());
-printIt(create(true));
-printIt(null);
-printIt(undefined);
